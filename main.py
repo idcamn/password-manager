cd3ed51734fbc67d12bbffafca58baf47480efae
idcamn <43856460+idcamn@users.noreply.github.com>
2026-02-28 20:52:27 +0300

    add main loop with bare funcs

--- main.py
+++ main.py
@@ -24,23 +24,47 @@ def generate_password(length: int = 12) -> str:
 
 def show_menu():
     print("\tLocal Password Manager")
-    print("\thelp - get available cmds")
-    input("> ")
+    print("Type 'help' to get available cmds")
 
 
 def show_help():
-    print("help - show help")
     print("list - view all passwords")
     print("add - add new password")
     print("update - update password")
     print("quit - quit")
-    print("")
+
+
+def show_list():
+    print("not implemented yet")
+
+
+def show_add():
+    print("not implemented yet")
+
+
+def show_update():
+    print("not implemented yet")
 
 
 def main():
     # add auth
     show_menu()
-
+    while True:
+        cmd = input('> ').lower()
+        if not cmd:
+            continue
+        if cmd == 'quit':
+            break
+        elif cmd == 'help':
+            show_help()
+        elif cmd == 'list':
+            show_list()
+        elif cmd == 'add':
+            show_add()
+        elif cmd == 'update':
+            show_update()
+        else:
+            print("Unknown command. Type 'help' to get available commands")
 
 if __name__ == '__main__':
     main()
