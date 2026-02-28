9487229cdaaccb2b91c1eff3cba4b6c0f41e076c
idcamn <43856460+idcamn@users.noreply.github.com>
2026-02-28 18:13:51 +0300

    some app structure

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
-print("Password Manager")
-print("Options:")
-print("help - show help")
-print("list - view all passwords")
-print("add - add new password")
-print("update - update password")
-print("delete - delete password")
-print("quit - quit")
-print("")
+import sqlite3
+import secrets
 
-input("> ")
+def show_menu():
+    print("\tLocal Password Manager")
+    print("\thelp - get available cmds")
+    input("> ")
+
+
+def show_help():
+    print("help - show help")
+    print("list - view all passwords")
+    print("add - add new password")
+    print("update - update password")
+    print("quit - quit")
+    print("")
+
+
+def main():
+    # add auth
+    show_menu()
+
+
+if __name__ == '__main__':
+    main()
